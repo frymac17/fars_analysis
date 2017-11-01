c05e36f5333332c0d57ec0b4e2f274a1987f3c3d
Mackenzie Fry <mfry@colostate.edu>
2017-11-01 10:37:04 -0600

    First function done

--- R/fars_functions.R
+++ R/fars_functions.R
@@ -1,31 +1,76 @@
-##functions
-clean_fars <- map_df(1999:2010, clean_yearly_person_file)
-save(clean_fars, file = "data/clean_fars.RData")
-load("data/clean_fars.RData")
-dim(clean_fars)
-length(unique(clean_fars$unique_id))
-summary(clean_fars)
+library(dplyr)
+library(tidyr)
+library(readr)
+library(maps)
+library(purrr)
+library(knitr)
+library(ggplot2)
+library(scales)
+
+load("../data/clean_fars.RData")
 
 
-clean_fars %>%
-  mutate(year_cat = cut(year, breaks = c(1999, 2002, 2006, 2010),
-                        labels = c("1999-2002", "2003-2006",
-                                   "2007-2010"),
-                        include.lowest = TRUE, right = TRUE)) %>%
-  filter(!is.na(sex)) %>%
-  group_by(drug_type, sex, year_cat) %>%
-  summarize(n_non_missing = sum(!is.na(positive_for_drug)),
-            positive_test = sum(positive_for_drug, na.rm = TRUE),
-            perc_positive = round(100 * positive_test / n_non_missing, 1)) %>%
-  select(drug_type, sex, year_cat, perc_positive) %>%
-  unite(sex_year_cat, sex, year_cat) %>%
-  spread(sex_year_cat, perc_positive) %>%
-  knitr::kable(col.names = c("Drug type", "F 1999-2002",
-                             "F 2003-2006", "F 2007-2010",
-                             "M 1999-2002", "M 2003-2006",
-                             "M 2007-2010"))
+###Write a function for proportions and confidence intervals, split by drug class in
+### 1999 and 2010
+perc_cis<-function (df)
+{
+  clean_fars %>% 
+    filter(year %in% c(1999,2010)) %>% 
+    filter(!is.na(sex)) %>%
+    group_by(drug_type, year) %>%
+    summarize(n_non_missing = sum(!is.na(positive_for_drug)),
+              positive_test = sum(positive_for_drug, na.rm = TRUE),
+              perc_positive = round(100 * positive_test / n_non_missing, 1)) %>%
+    mutate(p = positive_test / n_non_missing)%>% 
+    mutate(pre_se = p * (1- p)) %>%
+    mutate(pre_se2 = pre_se / n_non_missing) %>% 
+    mutate(se = sqrt(pre_se2)) %>%
+    mutate(CI_LOWER = (p - 1.96 * se))%>% 
+    mutate(CI_UPPER = (p + 1.96 * se))%>%
+    mutate(CI_LOWER = round(CI_LOWER, digits = 3)) %>% 
+    mutate(CI_UPPER = round(CI_UPPER, digits = 3)) %>% 
+    mutate(CI_UPPER = percent(CI_UPPER)) %>%
+    mutate(CI_LOWER = percent(CI_LOWER)) %>% 
+    mutate(p = round(p, digits = 3)) %>% 
+    mutate(p = percent(p)) %>% 
+    select(drug_type, year, p, CI_LOWER, CI_UPPER) %>% 
+    rename(Estimate = p) %>% 
+    unite(col = Confidence_Interval, CI_LOWER, CI_UPPER, sep = ", ") %>% 
+    unite(col = Values, Estimate, Confidence_Interval, sep = "  ") %>% 
+    spread(key = year, value = Values)
+}
+perc_cis()
 
 
 
 
+## write function a function that will input the clean_fars data set and drug. The column will output
+##a one row table of the Z statistic for the Cochran-Armitage trend over the 12 study years
+## and one column for p-value
 
+
+
+to_test <- clean_fars %>%
+  filter(drug_type == "Alcohol") %>%
+  group_by(year) %>%
+  summarize(positive = sum(positive_for_drug, na.rm = TRUE),
+            trials = sum(!is.na(positive_for_drug)))
+ca_alcohol <- prop.trend.test(x = to_test$positive,
+                              n = to_test$trials)
+sqrt(ca_alcohol$statistic)
+
+test_trend_ca(drug = "Stimulant", data = clean_fars)
+test_trend_ca(drug = "Alcohol")
+test_trend_ca(drug = "Nonalcohol")
+
+  drug_list <- c("Alcohol", "Nonalcohol", "Narcotic", "Depressant",
+                 "Stimulant", "Cannabinoid", "Other")
+  drug_trend_tests_ca <- lapply(drug_list, test_trend_ca)
+  drug_trend_tests_ca <- dplyr::bind_rows(drug_trend_tests_ca) %>%
+    dplyr::mutate(drug = drug_list) %>%
+    dplyr::select(drug, Z, p.value)
+  drug_trend_tests_ca %>% knitr::kable()
+  
+  
+
+### write a functionto fit the logistic model shown above to a specific drug category
